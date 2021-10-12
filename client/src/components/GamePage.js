ca53fe28224e80f300a641f890166f23b4809a63
Daniel Jowett <dbjowett@gmail.com>
2021-10-12 21:08:48 +0900

    Covert Game Page into grid

--- client/src/components/GamePage.js
+++ client/src/components/GamePage.js
@@ -25,38 +25,42 @@ const GamePage = ({ match }) => {
         </div>
       </div>
       <div className='container'>
-        <div className='summaryGameInfo'>
-          <div className='gameInfo'>
-            <ul className='gameInfoItems'>
-              <h2>Game Info</h2>
-              {currentGame.total_rating ? <li className='info'>Rating: {Math.round(currentGame.total_rating)}%</li> : null}
-              <li className='info'>Release: {currentGame.release_dates ? currentGame.release_dates[0].human : currentGame.release_dates[0].human}</li>
-              <li className='info'>Players: 1</li>
-              <h2>Genres</h2>
-              <ul>{renderedGenres}</ul>
-            </ul>
-            <ul className='platformInfo'>
-              <h2>Platforms</h2>
-              {renderedPlatforms}
-            </ul>
-          </div>
-          <div className='gameSummary'>
-            <h2>Summary</h2>
-            <div>{currentGame.summary}</div>
-          </div>
+        <div className='div1'>
+          <ul className='gameInfoItems'>
+            <h2>Game Info</h2>
+            {currentGame.total_rating ? <li className='info'>Rating: {Math.round(currentGame.total_rating)}%</li> : null}
+            <li className='info'>Release: {currentGame.release_dates ? currentGame.release_dates[0].human : currentGame.release_dates[0].human}</li>
+            <li className='info'>Players: 1</li>
+          </ul>
         </div>
-        <div className='mediaInfo'>
-          <div className='screenshots'>
-            <h2>Screenshots</h2>
-            {renderedImages}
-          </div>
-          {currentGame.videos ? (
-            <div className='videos'>
-              <h2>Videos</h2>
-              {renderedVideos}
-            </div>
-          ) : null}
+
+        <div className='div2'>
+          <h2>Genres</h2>
+          <ul>{renderedGenres}</ul>
         </div>
+        <div className='div3'>
+          <ul className='platformInfo'>
+            <h2>Platforms</h2>
+            {renderedPlatforms}
+          </ul>
+        </div>
+        <div className='div4'></div>
+
+        <div className='screenshots div5'>
+          <h2>Screenshots</h2>
+          {renderedImages}
+        </div>
+        <div className='div6'>
+          <h2>Summary</h2>
+          <div>{currentGame.summary}</div>
+        </div>
+        <div className='div7'></div>
+        {currentGame.videos ? (
+          <div className='videos div8'>
+            <h2>Videos</h2>
+            {renderedVideos}
+          </div>
+        ) : null}
       </div>
     </div>
   );
