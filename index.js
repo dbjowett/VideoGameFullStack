939826440e41891dc76a879b749cd5a0c210738a
Daniel Jowett <dbjowett@gmail.com>
2021-10-17 18:45:46 +0900

    Add MongoDB

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 require('./services/passport');
-const PORT = process.env.PORT || 5000;
+require('./models/User');
+
+const keys = require('./config/keys');
 
 const app = express();
+mongoose.connect(keys.mongoURI);
 
 require('./routes/userAuthRoutes')(app);
 require('./routes/upcoming')(app);
 require('./routes/search')(app);
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
