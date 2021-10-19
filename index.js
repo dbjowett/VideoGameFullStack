0af168e3d71427d1435a19232822fbf20a25b1ed
Daniel Jowett <dbjowett@gmail.com>
2021-10-20 06:31:50 +0900

    Deploying

--- index.js
+++ index.js
@@ -20,14 +20,5 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-app
-  .get('/', function (request, response) {
-    var result = 'App is running';
-    response.send(result);
-  })
-  .listen(app.get('port'), function () {
-    console.log('App is running, server is listening on port ', app.get('port'));
-  });
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
