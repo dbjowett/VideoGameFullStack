300119700eb953d803cf9a2847d5fb23f3806020
Daniel Jowett <dbjowett@gmail.com>
2021-10-20 13:15:10 +0900

    Deploying

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 // const mongoose = require('mongoose');
-const path = require('path');
-const keys = require('./config/keys');
+// const keys = require('./config/keys');
 
 require('./models/User');
 require('./services/passport');
@@ -9,12 +8,13 @@ require('./services/passport');
 // mongoose.connect(process.env.mongoURI || keys.mongoURI);
 
 const app = express();
-require('./routes/userAuthRoutes')(app);
+// require('./routes/userAuthRoutes')(app);
 require('./routes/upcoming')(app);
 require('./routes/search')(app);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
+  const path = require('path');
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
