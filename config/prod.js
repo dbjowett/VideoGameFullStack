758c1859b3bdb310bca697128d57be176bce5dc5
Daniel Jowett <dbjowett@gmail.com>
2021-10-20 13:04:43 +0900

    Deploying

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
-  id: process.env.TWITCH_CLIENT_ID,
-  secret: process.env.TWITCH_CLIENT_SECRET,
+  twitch_id: process.env.TWITCH_CLIENT_ID,
+  twitch_secret: process.env.TWITCH_CLIENT_SECRET,
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI
